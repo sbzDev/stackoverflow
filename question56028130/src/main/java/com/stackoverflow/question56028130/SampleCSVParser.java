c99bf68910595dd2e2077a77c7193d08c98b82cb
sbz <bzhezitskiy@gmail.com>
2019-05-08 10:21:20 +0300

    https://stackoverflow.com/questions/56028130/regex-for-parsing-csv-that-contains-json-in-a-column

--- question56028130/src/main/java/com/stackoverflow/question56028130/SampleCSVParser.java
+++ question56028130/src/main/java/com/stackoverflow/question56028130/SampleCSVParser.java
@@ -1,6 +1,5 @@
 package com.stackoverflow.question56028130;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.apache.commons.lang3.StringUtils;
@@ -9,15 +8,12 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Path;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 public class SampleCSVParser {
-    static final String[] HEADERS = {"id", "apptoolID", "apptoolUUID", "accountNumber", "accountName", "name", "description", "mac", "status","pGroups", "oemCode", "oemTagList", "locationID", "userCode", "businessUnit", "customerDescription", "notification", "ptdd", "hdptdd", "ptddSchedule", "compLive", "optInfo", "config1", "scriptDebugging", "clearLocalStorage", "created_at", "updated_at"};
-
     public List<Map<String, String>> parse(Path csvPath) throws IOException {
         Reader in = new FileReader(csvPath.toFile());
 
